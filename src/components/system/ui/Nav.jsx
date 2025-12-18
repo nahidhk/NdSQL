f9b9ed10d469bf16c20c933ba3d3fd3a9d55d53b
Nahid HK <nahidappltd@gmail.com>
2025-12-18 21:12:08 +0600

    updaye jsx

--- src/components/system/ui/Nav.jsx
+++ src/components/system/ui/Nav.jsx
@@ -1,9 +1,17 @@
-import react from "react"
-
+import react from "react";
+import Nav_button from "/Nav_button"
+//import navLogo from "../../assest/img/nav_logo.png"
 export default function Nav(){
   return(
     <>
-       <h1>Nav</h1>
+       <div className="nav">
+         <span>
+           NdSQK
+         </span>
+         <div>
+          <Nav_button /> 
+         </div>
+       </div>
     </>
     )
 }
---
+++ src/components/system/ui/Nav_button.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Nav_button(){
+  return(
+    <>
+      Nav Button
+    </>
+    )
+}
