c98cae5a87e8b1bdae227faa3a0668f7568dd47e
NAHID HK <nahidappltd@gmail.com>
2026-02-19 04:58:17 +0600

    new update

--- src/components/system/Nav.jsx
+++ src/components/system/Nav.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/img/nav_logo.png"
 import navComponents from "../../data/navComponent.json"
 import { useNavigate } from "react-router-dom";
 import { HiMenuAlt1 } from "react-icons/hi";
+import { RiCloseLargeFill } from "react-icons/ri";
 
 export default function Nav() {
+    const [dispaly, setDispaly] = useState("");
+    const [menuICon, setMenuIcon] = useState(true);
     const navigate = useNavigate();
+    const togelMenu = () => {
+
+        if (!menuICon) {
+            setMenuIcon(true);
+            setDispaly("none");
+        } else {
+            setMenuIcon(false);
+            setDispaly("flex");
+        }
+    }
     return (
         <>
             <div className="nav flex around medel">
@@ -13,8 +26,17 @@ export default function Nav() {
                     <img src={logo} className="nav-logo" />
                 </div>
                 <div className="nav-btnBox">
-                   <HiMenuAlt1 className="mobileNavButton"/>
-                    <ul className="nav-list flex center medel">
+                    <div onClick={togelMenu}>
+                        {
+                            menuICon ? (
+                                <HiMenuAlt1 className="mobileNavButton" />
+                            ) : (
+                                <RiCloseLargeFill className="mobileNavButton" />
+                            )
+                        }
+                    </div>
+
+                    <ul style={{ display: dispaly }} className={`nav-list flex center medel`}>
                         {
                             navComponents.map(item => (
                                 <li onClick={() => navigate(item.target)} key={item.id}>{item.title}</li>
